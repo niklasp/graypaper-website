91bb0080d730835a0362a4d446e98180a3460a6e
Niklas P <contact@niklasplessing.net>
2024-07-27 22:18:48 +0200

    wip

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -5,13 +5,19 @@ import { GatsbyNode } from "gatsby"
 
 const fs = require("fs")
 
-export const onPostBuild = () => {
-  fs.copyFile("./src/data/clients.json", "./public/clients.json", (err) => {
-    if (err) throw err
-  })
+export const onPostBuild: GatsbyNode["onPostBuild"] = () => {
+  fs.copyFile(
+    "./src/data/clients.json",
+    "./public/clients.json",
+    (err: NodeJS.ErrnoException | null) => {
+      if (err) throw err
+    },
+  )
 }
 
-export const onCreateWebpackConfig = ({ actions }) => {
+export const onCreateWebpackConfig: GatsbyNode["onCreateWebpackConfig"] = ({
+  actions,
+}) => {
   actions.setWebpackConfig({
     module: {
       rules: [
@@ -24,7 +30,7 @@ export const onCreateWebpackConfig = ({ actions }) => {
   })
 }
 
-export const createPages = async ({ actions }) => {
+export const createPages: GatsbyNode["createPages"] = async ({ actions }) => {
   console.info("Creating pages")
   lectures.forEach((lecture) => {
     actions.createPage({
